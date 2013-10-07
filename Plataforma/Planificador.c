4a346dd95b9761402153f76e76383d27c738bd32
cesarvera <cesar.vera96@gmail.com>
2013-10-07 17:03:07 -0300

    agrego los archivos del planificador

---
+++ Plataforma/Planificador.c
@@ -0,0 +1,12 @@
+/*
+ * Planificador.c
+ *
+ *  Created on: 07/10/2013
+ *      Author: utnso
+ */
+
+#include "Planificador.h"
+
+void planificador(datos_planificador_t *datos) {
+
+}
---
+++ Plataforma/Planificador.h
@@ -0,0 +1,15 @@
+/*
+ * Planificador.h
+ *
+ *  Created on: 07/10/2013
+ *      Author: utnso
+ */
+
+#ifndef PLANIFICADOR_H_
+#define PLANIFICADOR_H_
+
+#include "Orquestador.h"
+
+void planificador(datos_planificador_t *datos);
+
+#endif /* PLANIFICADOR_H_ */
