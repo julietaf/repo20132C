2bd6137db02e1d8626d2a9d49c860c170be222d7
Diego Vidal Pena <vidalpenadiego@gmail.com>
2013-11-17 11:19:01 -0300

    Primera parte del FileSystem

---
+++ FileSystem/Debug/Fuse/t_disco.d
@@ -0,0 +1,8 @@
+Fuse/t_disco.d: ../Fuse/t_disco.c ../Fuse/t_disco.h ../Fuse/grasa.h \
+ /home/utnso/workspace/commons/commons/log.h
+
+../Fuse/t_disco.h:
+
+../Fuse/grasa.h:
+
+/home/utnso/workspace/commons/commons/log.h:
