d7347ba72fe999fa9bcc1d44d3a51479acb93545
cesarvera <cesar.vera96@gmail.com>
2013-11-29 11:35:33 -0300

    pusheo de makefiles

--- commons/Debug/commons/subdir.mk
+++ commons/Debug/commons/subdir.mk
@@ -6,30 +6,45 @@
 C_SRCS += \
 ../commons/bitarray.c \
 ../commons/config.c \
+../commons/deadlock_detection.c \
 ../commons/error.c \
+../commons/geospatial.c \
 ../commons/log.c \
+../commons/nivel-gui.c \
 ../commons/process.c \
+../commons/sockets.c \
 ../commons/string.c \
+../commons/tad_items.c \
 ../commons/temporal.c \
 ../commons/txt.c 
 
 OBJS += \
 ./commons/bitarray.o \
 ./commons/config.o \
+./commons/deadlock_detection.o \
 ./commons/error.o \
+./commons/geospatial.o \
 ./commons/log.o \
+./commons/nivel-gui.o \
 ./commons/process.o \
+./commons/sockets.o \
 ./commons/string.o \
+./commons/tad_items.o \
 ./commons/temporal.o \
 ./commons/txt.o 
 
 C_DEPS += \
 ./commons/bitarray.d \
 ./commons/config.d \
+./commons/deadlock_detection.d \
 ./commons/error.d \
+./commons/geospatial.d \
 ./commons/log.d \
+./commons/nivel-gui.d \
 ./commons/process.d \
+./commons/sockets.d \
 ./commons/string.d \
+./commons/tad_items.d \
 ./commons/temporal.d \
 ./commons/txt.d 
 
