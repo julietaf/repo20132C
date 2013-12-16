474889f011b7e2243e32ecb782bf1811c10a2202
Javier Fernandez <j.s.fernandez88@gmail.com>
2013-12-16 00:21:13 -0300

    Operaciones read, open, readdir y gatattr (sin testiar) elimino basura

--- FileSystem/Debug/Fuse/t_disco.d
+++
@@ -1,8 +0,0 @@
-Fuse/t_disco.d: ../Fuse/t_disco.c ../Fuse/t_disco.h ../Fuse/grasa.h \
- /home/utnso/workspace/commons/commons/log.h
-
-../Fuse/t_disco.h:
-
-../Fuse/grasa.h:
-
-/home/utnso/workspace/commons/commons/log.h:
